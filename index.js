10e64a09df158a5b04969e48b3fa5ab1e9adfcb1
AlexSkir <a.skirnevskaia@gmail.com>
2020-05-07 02:42:49 +0300

    add: add server

--- index.js
+++ index.js
@@ -24,9 +24,21 @@ function writeMessage(messageBody) {
   database.db.ref().child('messages').push(messageBody);
 }
 
-const WebSocket = require('ws')
+// const WebSocket = require('ws')
 
-const wss = new WebSocket.Server({ port: 8080 })
+// const wss = new WebSocket.Server({ port: 8080 })
+
+const express = require('express');
+const { Server } = require('ws');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
 
 wss.on('connection', (ws) => {
   database.db.ref('/messages/').once('value').then(function (snapshot) {
