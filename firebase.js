3f5286f21173cb7cf9536c8dd28c528f97d5b669
AlexSkir <a.skirnevskaia@gmail.com>
2023-04-05 19:24:32 +0300

    update env

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 var firebase = require('firebase');
 var firebaseConfig = {
-  apiKey: "AIzaSyDzthWJFYauBksaK3P76YC3UBJroS-o_dE",
-  authDomain: "awesome-chat-ws.firebaseapp.com",
-  databaseURL: "https://awesome-chat-ws.firebaseio.com",
-  projectId: "awesome-chat-ws",
-  storageBucket: "awesome-chat-ws.appspot.com",
-  messagingSenderId: "714601216366",
-  appId: "1:714601216366:web:669876390fcbb69d07297f"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 };
 firebase.initializeApp(firebaseConfig)
 
